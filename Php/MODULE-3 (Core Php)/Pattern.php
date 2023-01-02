577e403d5fee82f7b19ffecd0d443ebb6a4a8359
yash dodiya <yashsinhrajput1@gmail.com>
2023-01-02 22:44:24 +0530

    Pattern.php

---
+++ Php/MODULE-3 (Core Php)/Pattern.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+
+Write a program for this Pattern:
+*****
+*
+*
+*
+*****
+
+*/
+
+for($i=1;$i<=5;$i++)
+{
+  if($i==1 or $i==5) 
+  {
+    echo "* * * * * ";
+    echo "<br>";
+  }
+  else
+  {
+    echo "*";
+    echo "<br>";
+  }
+    
+}
+?>
