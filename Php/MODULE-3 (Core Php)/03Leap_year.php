73def269f0ff2bc450cd7f0d8c68f693f23eb19c
yash dodiya <yashsinhrajput1@gmail.com>
2023-01-04 22:47:56 +0530

    Leap_year

---
+++ Php/MODULE-3 (Core Php)/03Leap_year.php
@@ -0,0 +1,14 @@
+<?php
+/*  Write a PHP program to check Leap years between 1901 to 2016 Using
+nested if. 
+*/
+for($a=1901;$a<=2016;$a++)
+{
+    // 
+    if($a%4==0)
+    {
+        echo "<br>$a  =  year is leap year ";
+    }
+   
+}
+?>
